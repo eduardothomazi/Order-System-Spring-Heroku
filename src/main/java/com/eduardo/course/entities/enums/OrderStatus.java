4077039b6f91421138c1b98a31ac82d4f36e198b
Eduardo Thomazi <eduardothomazi91@gmail.com>
2021-11-18 13:52:01 -0300

    C2 - OrderStatus enum

---
+++ src/main/java/com/eduardo/course/entities/enums/OrderStatus.java
@@ -0,0 +1,29 @@
+package com.eduardo.course.entities.enums;
+
+import com.eduardo.course.entities.Order;
+
+public enum OrderStatus {
+    WAITING_PAYMENT (1),
+    PAID(2),
+    SHIPPED(3),
+    DELIVERED(4),
+    CANCELED(5);
+
+    private int code;
+
+    private OrderStatus(int i) {
+        code = i;
+    }
+
+    public int getCode() {
+        return code;
+    }
+    public static OrderStatus valueOf(int code){
+        for (OrderStatus value : OrderStatus.values()){
+            if (value.getCode() == code){
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid OderStatus code!");
+    }
+}
