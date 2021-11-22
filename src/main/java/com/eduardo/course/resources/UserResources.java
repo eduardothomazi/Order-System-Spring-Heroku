2e71c4682604ff92a079f55107e0014161b60fe8
Eduardo Thomazi <eduardothomazi91@gmail.com>
2021-11-22 14:44:44 -0300

    C10 - User delete

--- src/main/java/com/eduardo/course/resources/UserResources.java
+++ src/main/java/com/eduardo/course/resources/UserResources.java
@@ -27,12 +27,16 @@ public class UserResources {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
     }
-
     @PostMapping
     public ResponseEntity<User> insert(@RequestBody User obj){
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+    }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
